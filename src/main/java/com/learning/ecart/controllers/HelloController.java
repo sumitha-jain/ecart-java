8079da9dc8517894d130150bda03b7d7bbd02c2d
[Sumitha Jain] <sumitha.allampalli@gmail.com>
2022-04-30 19:42:06 -0500

    intial commit

---
+++ src/main/java/com/learning/ecart/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.learning.ecart.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!!";
+    }
+}
